7b4000eaf0ad71e05f1944207413efcd5998b27d
Naw May Thu Han <nawmaythuhan@my.jcu.edu.au>
2024-03-16 16:09:20 +0800

    CP1404_Prac05_Wimbledon_Updated_Program_Commit

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,35 +1,31 @@
-"""
-Word Occurrences
-Estimate: 30 minutes
-Actual: minutes
-"""
-
 FILENAME = "wimbledon.csv"
 
 def main():
+    """Read and display champions and winning countries information"""
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         data = in_file.readlines()
-    champions = extract_champions(data)
-    countries = extract_countries(data)
+    champions = extract_data(data, 2)
+    countries = extract_data(data, 1)
     country_counts = count_items(countries)
     champion_counts = count_items(champions)
 
     print("Wimbledon Champions:")
-    for name in champion_counts.keys():
+    for name in sorted(champion_counts.keys()):
         print(f"{name}: {champion_counts[name]}")
+
     sorted_countries = sorted(country_counts.keys())
-    print(f"These {len(sorted_countries)} countries have won the Wimbledon:")
+    print(f"These {len(sorted_countries)} countries have won Wimbledon:")
     print(", ".join(sorted_countries))
 
-def extract_champions(data):
-    champions = [line.strip().split(",")[2] for line in data[1:]]
-    return champions
-
-def extract_countries(data):
-    countries = [line.strip().split(",")[1] for line in data[1:]]
-    return countries
+def extract_data(data, number):
+    """Get the data and number"""
+    items = []
+    for line in data[1:]:
+        items.append(line.strip().split(",")[number])
+    return items
 
 def count_items(items):
+    """Get the item counts"""
     item_counts = {}
     for item in items:
         if item in item_counts:
