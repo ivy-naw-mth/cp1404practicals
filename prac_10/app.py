0c91d3a1bf16fd9448a301036ecd34ba79764e3c
Naw May Thu Han <nawmaythuhan@my.jcu.edu.au>
2024-04-28 21:11:34 +0800

    CP1404_Prac10_Updated_AppCodes_Commit

--- prac_10/app.py
+++ prac_10/app.py
@@ -14,14 +14,14 @@ def greet(name=""):
     return f"Hello {name}"
 
 
-@app.route('/c/<fahrenheit>')
-def convert_to_celsius(fahrenheit):
+@app.route('/fahrenheit')
+def convert_to_celsius(fahrenheit=23):
     celsius = 5 / 9 * (float(fahrenheit) - 32)
     return str(celsius)
 
 
 @app.route('/f/<celsius>')
-def convert_to_fahrenheit(celsius):
+def convert_to_fahrenheit(celsius=11):
     fahrenheit = float(celsius) * 9.0 / 5 + 32
     return str(fahrenheit)
 
